b53d5d35ac8a1d3d11e522ff84446fae32b2e2cc
raman <meetraman16@outlook.com>
2017-05-02 16:14:04 +0530

    scrum added

--- src/app/dashboard/dashboard.routing.js
+++ src/app/dashboard/dashboard.routing.js
@@ -4,14 +4,16 @@ var router_1 = require("@angular/router");
 var dashboard_component_1 = require("./dashboard.component");
 var resources_routing_1 = require("../resources/resources.routing");
 var resources_component_1 = require("../resources/resources.component");
+var scrum_component_1 = require("./scrum.component");
 var routes = [
     { path: 'dashboard', component: dashboard_component_1.DashboardComponent, children: [
-            { path: 'resources', component: resources_component_1.ResourcesComponent, children: resources_routing_1.resourcesRoutes }
+            { path: 'resources', component: resources_component_1.ResourcesComponent, children: resources_routing_1.resourcesRoutes },
+            { path: 'scrums', component: scrum_component_1.ScrumComponent }
         ]
     }
 ];
 exports.dashboardRouting = {
     routes: router_1.RouterModule.forChild(routes),
-    components: [dashboard_component_1.DashboardComponent, resources_component_1.ResourcesComponent]
+    components: [dashboard_component_1.DashboardComponent, resources_component_1.ResourcesComponent, scrum_component_1.ScrumComponent]
 };
 //# sourceMappingURL=dashboard.routing.js.map
