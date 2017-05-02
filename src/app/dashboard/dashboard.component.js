896129e52d00d4325eafb218e9cadc3a1534622f
raman <meetraman16@outlook.com>
2017-05-02 16:35:17 +0530

    graphs added

--- src/app/dashboard/dashboard.component.js
+++ src/app/dashboard/dashboard.component.js
@@ -5,11 +5,83 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var DashboardComponent = (function () {
     function DashboardComponent() {
     }
+    DashboardComponent.prototype.ngOnInit = function () {
+        Highcharts.chart('chart2', {
+            title: {
+                text: 'Employee Records'
+            },
+            yAxis: {
+                title: {
+                    text: 'Number of Employees'
+                }
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle'
+            },
+            plotOptions: {
+                series: {
+                    pointStart: 2009
+                }
+            },
+            series: [{
+                    name: 'Scrum 1',
+                    data: [43, 52, 57, 69, 97, 119, 137, 154]
+                }, {
+                    name: 'Scrum 2',
+                    data: [24, 24, 29, 29, 32, 30, 38, 40]
+                }, {
+                    name: 'Scrum 3',
+                    data: [11, 17, 16, 19, 20, 24, 32, 39]
+                }]
+        });
+        Highcharts.chart('chart1', {
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: 'Resources'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: false
+                    },
+                    showInLegend: true
+                }
+            },
+            series: [{
+                    name: 'Brands',
+                    colorByPoint: true,
+                    data: [{
+                            name: 'SI',
+                            y: 56.33
+                        }, {
+                            name: 'AO',
+                            y: 24.03,
+                            sliced: true,
+                            selected: true
+                        }]
+                }]
+        });
+    };
     return DashboardComponent;
 }());
 DashboardComponent = __decorate([
@@ -17,7 +89,8 @@ DashboardComponent = __decorate([
         moduleId: module.id,
         selector: 'dashboard-container',
         templateUrl: 'dashboard.component.html',
-    })
+    }),
+    __metadata("design:paramtypes", [])
 ], DashboardComponent);
 exports.DashboardComponent = DashboardComponent;
 //# sourceMappingURL=dashboard.component.js.map
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,99 @@
-import {Component,Input,Output,EventEmitter } from '@angular/core';
+import {Component,Input,Output,EventEmitter,OnInit } from '@angular/core';
 import {Location} from '@angular/common';
 import { DataService } from '../core/services/data.service';
 import {Response} from '@angular/http';
-
+declare var Highcharts:any;
 @Component({ 
   moduleId: module.id,
   selector: 'dashboard-container', 
   templateUrl: 'dashboard.component.html',
 })
 
-export class DashboardComponent{
+export class DashboardComponent implements OnInit {
+
+
+ngOnInit(){
+  Highcharts.chart('chart2', {
+
+    title: {
+        text: 'Employee Records'
+    },
+    yAxis: {
+        title: {
+            text: 'Number of Employees'
+        }
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle'
+    },
+
+    plotOptions: {
+        series: {
+            pointStart: 2009
+        }
+    },
+
+    series: [{
+        name: 'Scrum 1',
+        data: [43, 52, 57, 69, 97, 119, 137, 154]
+    }, {
+        name: 'Scrum 2',
+        data: [24, 24, 29, 29, 32, 30, 38, 40]
+    }, {
+        name: 'Scrum 3',
+        data: [11, 17, 16, 19, 20, 24, 32, 39]
+    }]
+
+});
+
+
+ Highcharts.chart('chart1', {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: 'Resources'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false
+                },
+                showInLegend: true
+            }
+        },
+        series: [{
+            name: 'Brands',
+            colorByPoint: true,
+            data: [{
+                name: 'SI',
+                y: 56.33
+            }, {
+                name: 'AO',
+                y: 24.03,
+                sliced: true,
+                selected: true
+            }]
+        }]
+    });
+}
+constructor(){
+  
+
+
+
+ 
+}
+ 
+
 }
